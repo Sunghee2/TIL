354db467c94829dd4c84f7bdd02312473f86058e
Sunghee2 <630sunghee@naver.com>
2018-05-17 23:08:30 +0900

    Todo ~ing

---
+++ react/Todo/components/task_input.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+export default class TaskInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = props.onSubmit;
+    this.state = {title: ""};
+    this.submitTask = this.submitTask.bind(this);
+  }
+
+  submitTask() {
+    this.onSubmit(this.state.title.trim());
+    this.setState({title: ""});
+    this.refs.txtInput.focus();
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView
+        style={{backgroundColor: '#eee'}}
+        behavior='padding'
+        keyboardVerticalOffset={0}>
+        <TextInput style={styles.textStyle}
+          autoFocus={true}
+          placeholder='type here!'
+          ref='txtInput'
+          value={this.state.title}
+          onChangeText={(text) => this.setState({title: text})}
+          onSubmitEditing={this.submitTask}
+        />
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    backgroundColor: '#fff',
+    height: 40, 
+    margin: 5,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#eee',
+    borderRadius: 10
+  }
+});
